4a1955119f287648775f8f2f3a143f9724f70af5
Paloma Cezário <147927123+PalomaCezario@users.noreply.github.com>
2025-09-21 20:34:54 -0300

    Finalizando os endpoints (Listar, criar, editar, deletar)

--- src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,23 +1,58 @@
 package br.com.criandoapi.projeto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.criandoapi.projeto.DAO.IUsuario;
 import br.com.criandoapi.projeto.model.Usuario;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
+@RequestMapping ("/usuarios")
 public class UsuarioController {
 
     // faça a importação dos metodos sem precisar escrever um por um 
     @Autowired
     private IUsuario dao;
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public List <Usuario> listaUsuarios (){
         return (List<Usuario>) dao.findAll();
     }
+
+    //cria novo usuario
+    @PostMapping
+    public Usuario criaUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;    
+    }
+    
+    //edita um usuario
+    @PutMapping
+    public Usuario editarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;    
+    }
+
+    //delete o usuario procurado através do id
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> excluirUsuario(@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+
+    }
+
+
+    
 }
